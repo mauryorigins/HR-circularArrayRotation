2d5abb1335dbf8f8ef0a115a40488ddf0bf2919c
mauryo <mauriciomorrison@hotmail.com>
2021-12-22 11:17:42 -0600

    Url exercise

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 /* eslint-disable no-return-assign */
 
 // Source: https://www.hackerrank.com/challenges/drawing-book/problem?isFullScreen=true
-import pageCount from './pageCount';
+import countingValleys from './countingValleys';
 
 function main() {
   console.log('---------- Cool Programing ----------\n');
-  const result = pageCount(5, 4);
+  const result = countingValleys(8, 'UDDDUDUU');
   console.log('Result: ', result);
 }
 
