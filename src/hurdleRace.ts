f9cc0c266e2d33a5fbeef6fc7c7fabc2195d94be
mauryo <mauriciomorrison@hotmail.com>
2021-12-23 15:28:32 -0600

    Url exercise

--- src/hurdleRace.ts
+++
@@ -1,9 +0,0 @@
-export default function hurdleRace(k: number, height: number[]): number {
-  // Write your code here
-  const max = Math.max(...height);
-  let res = 0;
-  if (max - k >= 0) {
-    res = max - k;
-  }
-  return res;
-}
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 /* eslint-disable no-return-assign */
 
-// Source: https://www.hackerrank.com/challenges/the-hurdle-race/problem?isFullScreen=true
-import hurdleRace from './hurdleRace';
+// Source: https://www.hackerrank.com/challenges/designer-pdf-viewer/problem?isFullScreen=true
+import designerPdfViewer from './designerPdfViewer';
 
 function main() {
   console.log('---------- Cool Programing ----------\n');
-  const result = hurdleRace(7, [2, 5, 4, 5, 2]);
+  const testAbc = [1, 3, 1, 3, 1, 4, 1, 3, 2, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 7];
+  const testWord = 'zaba';
+  const result = designerPdfViewer(testAbc, testWord);
   console.log('Result: ', result);
 }
 
