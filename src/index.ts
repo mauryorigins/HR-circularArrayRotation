5b27e387c3bb50756604e6cbd8a945450b8793c6
mauryo <mauriciomorrison@hotmail.com>
2021-12-27 13:15:27 -0600

    Url exercise

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 /* eslint-disable no-return-assign */
 
-// Source: https://www.hackerrank.com/challenges/strange-advertising/problem?isFullScreen=true
-import viralAdvertising from './viralAdvertising';
+// Source: https://www.hackerrank.com/challenges/save-the-prisoner/problem?isFullScreen=true
+import saveThePrisoner from './saveThePrisoner';
 
 function main() {
   console.log('---------- Cool Programing ----------\n');
-  const result = viralAdvertising(5);
+  const result = saveThePrisoner(7, 19, 2);
   console.log('Result: ', result);
 }
 
